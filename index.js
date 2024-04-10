bc1b317bab1b2ef0f320000bc6a338ca5d38147a
GuilhermeZampar <145402235+Gui-Z07@users.noreply.github.com>
2024-04-10 04:14:51 -0300

    update main and new aside scroll

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-const spans = document.querySelectorAll('.pricipal-titles-main');
+const spans = document.querySelectorAll(".pricipal-titles-main");
 spans.forEach((article, index) => {
-    article.style.setProperty('--delay', index);
+  article.style.setProperty("--delay", index);
 });
+
+var aside = document.getElementById("barraRolagem");
+// Nessa função verificaremos se o conteudo que colocamos no html do aside, vai exceder a altura que temos definida no css, no caso h=100%
+function ajustarScroll() {
+  if (section.scrollHeight > aside.clientHeight) {
+    // Se o conteúdo exceder a altura do monitor, exibiremos o scroll
+    aside.style.overflowY = "scroll";
+  } else {
+    // Se o conteúdo não exceder a altura, ocultaremos o scroll
+    aside.style.overflowY = "hidden";
+  }
+}
+
+// Esta função irá ajustar nosso scroll quando a pag for carregada
+window.addEventListener("load", ajustarScroll);
+
+// Aqui um resize, essa função ajuda no ajuste sempre que o tamanho da janela for alterado (bom para mq)
+window.addEventListener("resize", ajustarScroll);
